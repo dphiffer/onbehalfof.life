7f5625590deacaea06b3449c04f3de24eedfabc0
Dan Phiffer <dan@phiffer.org>
2018-02-14 15:51:37 -0500

    :sparkles: first

---
+++ life.js
@@ -0,0 +1,115 @@
+var src_list = [];
+var canvas, ctx, species;
+var ignore_hashchange = false;
+
+function choose(reload) {
+	var url = 'species.php';
+	if (reload) {
+		url += '?reload=1';
+	}
+	$.get(url, function(rsp) {
+		species = rsp.species;
+		ignore_hashchange = true;
+		window.location = '#species' + species.id;
+		init();
+	});
+}
+
+function imgload() {
+	var src = src_list.shift();
+	console.log('imgload: ' + src);
+	var img = document.createElement("img");
+	img.onload = function() {
+		console.log('onload');
+		imghandle(img);
+	};
+	img.setAttribute('crossOrigin', 'anonymous');
+	img.src = src;
+	$('#custom').append(img);
+}
+
+function imghandle(img) {
+	console.log(img);
+	var w = 640;
+	var h = Math.ceil(640 * $(img).height() / $(img).width());
+	if (h < 640) {
+		var offset = Math.floor((640 - h) / 2);
+		ctx.drawImage(img, 0, offset - h, w, h);
+		ctx.drawImage(img, 0, offset, w, h);
+		ctx.drawImage(img, 0, 639 - offset, w, h);
+	} else {
+		ctx.drawImage(img, 0, 0, w, h);
+	}
+	if (src_list.length > 0) {
+		imgload();
+	} else {
+		var data_uri = canvas.toDataURL();
+		$('#share-img').html('<figure><img><figcaption><a href="https://www.inaturalist.org/taxa/' + species.id + '" target="_blank"><i>' + species.latin + '</i> on iNaturalist.org</a>.<div class="attr">PHOTO: <a href="' + species.photo_url + '" target="_blank">' + species.photo_attr + '</a></div></figcaption></figure>');
+		$('#share-img img')[0].src = data_uri;
+		if ($('#share-img2').length > 0) {
+			$('#share-img2')[0].src = data_uri;
+		} else {
+			$('<img id="share-img2">').insertAfter('#share h2');
+			$('#share-img2')[0].src = data_uri;
+		}
+		$('#reload').removeClass('loading');
+	}
+}
+
+function load(id) {
+	console.log('load ' + id);
+	$.get('species.php?id=' + id, function(rsp) {
+		species = rsp.species;
+		init();
+	});
+}
+
+function init() {
+	var url = 'https://www.inaturalist.org/taxa/' + species.id;
+	var link = '<a href="' + url + '" target="_blank">' + species.common.toUpperCase() + ' (<i>' + species.latin + '</i>)</a>';
+	$('#life').html(link);
+	$('#another').html(link);
+	src_list.push('species/' + species.id + '.jpg');
+	src_list.push('media/onbehalfof.life-template.png');
+	$('#custom').html('<canvas width="640" height="640"></canvas>');
+	canvas = $('#custom canvas')[0];
+	ctx = canvas.getContext('2d');
+	imgload();
+}
+
+$(document).ready(function() {
+	var hash = location.hash.match(/^#species(\d+)$/);
+	if (hash) {
+		var id = hash[1];
+		load(id);
+	} else {
+		choose();
+	}
+
+	window.onhashchange = function() {
+		if (ignore_hashchange) {
+			ignore_hashchange = false;
+			return;
+		}
+		var hash = location.hash.match(/^#species(\d+)$/);
+		if (hash) {
+			var id = hash[1];
+			console.log('load ' + id);
+			load(id);
+		}
+	};
+
+	$('#reload').click(function(e) {
+		e.preventDefault();
+		if ($('#reload').hasClass('loading')) {
+			return;
+		}
+		$('#reload').addClass('loading');
+		console.log(e.shiftKey);
+		choose(e.shiftKey);
+	});
+
+	$.get('species.html', function(rsp) {
+		$('#all-species').html(rsp);
+	});
+});
---
+++ species.php
@@ -0,0 +1,87 @@
+<?php
+
+ini_set('display_errors', 0);
+$photo = null;
+$species = null;
+species();
+
+function species() {
+	global $photo, $species;
+	if (empty($_GET['id'])) {
+		if (empty($_GET['reload'])) {
+			$files = glob('species/*.json');
+			$ids = array();
+			foreach ($files as $file) {
+				if (preg_match('/(\d+)\.json/', $file, $matches)) {
+					$ids[] = $matches[1];
+				}
+			}
+			$index = rand(0, count($ids) - 1);
+			$id = $ids[$index];
+		} else {
+			$json = file_get_contents('species.json');
+			$species = json_decode($json, 'as hash');
+			$max = count($species['ids']);
+			$index = rand(0, $max - 1);
+			$id = $species['ids'][$index];
+		}
+	} else {
+		$id = intval($_GET['id']);
+	}
+
+	if (file_exists("species/$id.jpg") &&
+	    file_exists("species/$id.json")) {
+		$photo = "species/$id.jpg";
+		$species_json = file_get_contents("species/$id.json");
+		$species = json_decode($species_json);
+		list($width, $height, $type, $attr) = getimagesize("species/$id.jpg");
+		if ($width < 500) {
+			unset($_GET['id']);
+			return species();
+		}
+	} else {
+		$json = file_get_contents("https://api.inaturalist.org/v1/taxa/$id");
+		$rsp = json_decode($json, 'as hash');
+		if ($rsp && $rsp['results'] && count($rsp['results']) > 0) {
+			if (! empty($rsp['results'][0]['default_photo'])) {
+			    $url = $rsp['results'][0]['default_photo']['medium_url'];
+				$photo_attr = $rsp['results'][0]['default_photo']['attribution'];
+				$photo_url = $rsp['results'][0]['default_photo']['url'];
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				$data = curl_exec($ch);
+				curl_close($ch);
+				file_put_contents("species/$id.jpg", $data);
+				list($width, $height, $type, $attr) = getimagesize("species/$id.jpg");
+				if ($width < 500) {
+					unset($_GET['id']);
+					return species();
+				}
+				$photo = "species/$id.jpg";
+			} else {
+				return species();
+			}
+			$species = array(
+				'id' => $id,
+				'latin' => $rsp['results'][0]['name'],
+				'common' => $rsp['results'][0]['preferred_common_name']
+			);
+			if (! empty($photo_attr)) {
+				$species['photo_attr'] = $photo_attr;
+			}
+			if (! empty($photo_url)) {
+				$species['photo_url'] = $photo_url;
+			}
+			$json = json_encode($species, JSON_PRETTY_PRINT);
+			file_put_contents("species/$id.json", $json);
+		}
+	}
+
+	header('Content-Type: application/json');
+	echo json_encode(array(
+		'species' => $species,
+		'photo' => $photo
+	));
+}
