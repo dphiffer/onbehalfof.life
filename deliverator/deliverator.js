40dc2a36a58cc0f05e1c5c7d781c21184248e77f
Dan Phiffer <dan@phiffer.org>
2018-06-30 14:43:06 -0400

    :snail: basic HTTPS server

--- deliverator/deliverator.js
+++ deliverator/deliverator.js
@@ -1,11 +1,39 @@
+const fs = require('fs');
+if (! fs.existsSync(__dirname + '/config.js')) {
+	console.log('Please set up config.js');
+	process.exit(1);
+}
+
+const config = require('./config.js');
+const express = require('express');
+const app = express();
+const body_parser = require('body-parser');
 const puppeteer = require('puppeteer');
+
+var server;
+if ('ssl' in config) {
+	let https_options = {};
+	for (let key in config.ssl) {
+		https_options[key] = fs.readFileSync(`${__dirname}/${config.ssl[key]}`);
+	}
+	server = require('https').createServer(https_options, app);
+} else {
+	server = require('http').createServer(app);
+}
+
+app.use(express.static('public'));
+app.use(body_parser.urlencoded({ extended: false }));
+
+const port = config.port || 5000;
+server.listen(port, () => {
+	console.log(`listening on *:${port}`);
+});
 
 function timeout(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-(async () => {
-	const url = 'https://www.regulations.gov/comment?D=HHS-OS-2018-0008-0001';
+/*async comment(url, comment) => {
 	const browser = await puppeteer.launch({
 		headless: false
 	});
@@ -18,7 +46,5 @@ function timeout(ms) {
 		textarea = await page.$('textarea');
 	}
 
-	await textarea.type('hello this is a test, just testing hello there.', { delay: 30 });
-
-
-})();
+	await textarea.type(comment);
+}*/
